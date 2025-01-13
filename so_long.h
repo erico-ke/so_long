3f816d8d642166b84f68bada8c457e2f6929f47e
erico-ke <eliasricokergaravat@gmail.com>
2025-01-13 17:22:49 +0100

    started .h

--- so_long.h
+++ so_long.h
@@ -1,19 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/13 16:19:44 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/01/13 16:24:10 by erico-ke         ###   ########.fr       */
+/*   Created: 2025/01/13 16:24:53 by erico-ke          #+#    #+#             */
+/*   Updated: 2025/01/13 16:30:16 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-	{
-	}
-	return (0);
-}
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include <libft.h>
+# include <MLX42.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+
+
+
+#endif
