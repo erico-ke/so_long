6e7368d06645973923d306e9a6d9dcb5405ead6f
erico-ke <eliasricokergaravat@gmail.com>
2025-02-04 20:06:02 +0100

    sa

--- src/window.c
+++ src/window.c
@@ -1,30 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   window.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/13 16:19:44 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/02/04 16:52:11 by erico-ke         ###   ########.fr       */
+/*   Created: 2025/02/04 16:53:11 by erico-ke          #+#    #+#             */
+/*   Updated: 2025/02/04 16:53:48 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(int argc, char **argv)
-{
-	t_map	*map;
-
-	if (argc != 2)
-		return (print_error("Please insert one argument, just one."));
-	map = ft_calloc(1, sizeof(t_map));
-	if (map_control(map, argv[1]) == EXIT_FAILURE)
-	{
-		free_all(map);
-		return (EXIT_FAILURE);
-	}
-	free_all(map);
-	
-	return (EXIT_SUCCESS);
-}
